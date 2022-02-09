b4b8e9bb26c76afa52832d541a1213a249cf8418
sbafsk <sebastian@occupier.com>
2022-02-08 22:57:55 -0300

    initial commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import {
+  Typography,
+  Container,
+  Button,
+  TextField,
+  Link,
+  Box
+} from '@mui/material'
+
+const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // leer datos
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          <Button type="submit" fullWidth variant="contained" sx={{ my: 3 }}>
+            Sign In
+          </Button>
+          <Link href="#" variant="body2">
+            Registro
+          </Link>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
+
+export default Login
