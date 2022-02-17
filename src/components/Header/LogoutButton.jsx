5950109d81994ba810995bb048810a020c449ef5
sbafsk <sebastian@occupier.com>
2022-02-17 02:45:03 -0300

    header

---
+++ src/components/Header/LogoutButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { IconButton } from '@mui/material'
+import { Logout } from '@mui/icons-material'
+
+import { onLogout } from '../../store/actions'
+
+const LogoutButton = () => {
+  const dispatch = useDispatch()
+
+  const onHandleLogout = () => {
+    sessionStorage.removeItem('myOrderAppUser')
+    dispatch(onLogout())
+  }
+  return (
+    <IconButton onClick={onHandleLogout}>
+      <Logout />
+    </IconButton>
+  )
+}
+
+export default LogoutButton
