d23445b490fd0accdb10986a677005820a4f93f4
sbafsk <sebastian@occupier.com>
2022-02-22 04:27:25 -0300

    order list

--- src/components/Dashboard/OrderList/OrderList.jsx
+++ src/components/Dashboard/OrderList/OrderList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {
   Typography,
   Table,
@@ -8,21 +9,31 @@ import {
   TableRow,
   Paper
 } from '@mui/material'
+
+import { onDeleteOrder } from '../../../store/actions'
+import { deleteOrder } from '../../../services/api'
 import OrderListItem from './OrderListItem'
 
-const tableColums = [
-  'Id',
-  'Origen',
-  'Destino',
-  'Distancia (km)',
-  'Precio',
-  'Completado',
-  'Borrar'
-]
+const tableColums = ['Origen', 'Destino', 'Distancia (km)', 'Precio', 'Borrar']
 
-const OrderList = ({ orders }) => {
+const OrderList = ({ orders, setSnackData }) => {
+  const user = useSelector((state) => state.userLogged)
+  const dispatch = useDispatch()
+  const handleDelete = async (id) => {
+    try {
+      await deleteOrder(id, user)
+      dispatch(onDeleteOrder(id))
+      setSnackData({ open: true, message: 'Envio borrado', variant: 'info' })
+    } catch (error) {
+      setSnackData({
+        open: true,
+        message: 'Error al intentar borrar',
+        variant: 'error'
+      })
+    }
+  }
   return (
-    <Paper>
+    <Paper sx={{ display: 'flex', justifyContent: 'center' }}>
       {orders?.length > 0 ? (
         <Table size="small">
           <TableHead>
@@ -36,13 +47,17 @@ const OrderList = ({ orders }) => {
           </TableHead>
           <TableBody>
             {orders.map((order) => (
-              <OrderListItem {...order} key={order.id} />
+              <OrderListItem
+                orderData={order}
+                handleDelete={handleDelete}
+                key={order.id}
+              />
             ))}
           </TableBody>
         </Table>
       ) : (
-        <Typography variant="subtitle" align="center">
-          No hay pedidos.
+        <Typography variant="subtitle" sx={{ padding: '100px' }}>
+          No tiene envios registrados.
         </Typography>
       )}
     </Paper>
--- src/components/Dashboard/OrderList/OrderListItem/OrderListItem.jsx
+++ src/components/Dashboard/OrderList/OrderListItem/OrderListItem.jsx
@@ -1,63 +1,24 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { TableCell, TableRow, IconButton, Checkbox } from '@mui/material'
+import { useSelector } from 'react-redux'
+import { TableCell, TableRow, IconButton } from '@mui/material'
 import { Delete as DeleteIcon } from '@mui/icons-material'
 
-import { onDeleteOrder, onCompleteOrder } from '../../../../store/actions'
-import { deleteOrder, completeOrder } from '../../../../services/api'
+const OrderListItem = ({ orderData, handleDelete }) => {
+  const { id, ciudad_destino, ciudad_origen, distancia, precio } = orderData
 
-const OrderListItem = ({
-  id,
-  ciudad_destino,
-  ciudad_origen,
-  categoria,
-  distancia,
-  precio,
-  completed
-}) => {
-  const userLogged = useSelector((state) => state.userLogged)
-  // todo getCityName
   const origen = useSelector((state) =>
-    state.cities.find((c) => Number(c.id) === ciudad_origen)
+    state.cities.find((c) => c.id === ciudad_origen)
   )
   const destino = useSelector((state) =>
-    state.cities.find((c) => Number(c.id) === ciudad_destino)
+    state.cities.find((c) => c.id === ciudad_destino)
   )
 
-  const dispatch = useDispatch()
-
-  const handleDelete = async (id) => {
-    try {
-      await deleteOrder(id, userLogged)
-      dispatch(onDeleteOrder(id))
-    } catch (error) {
-      alert(error.message)
-    }
-  }
-
-  const handleComplete = async (id) => {
-    try {
-      //await completeOrder(id, userLogged)
-      dispatch(onCompleteOrder(id))
-    } catch (error) {
-      alert(error.message)
-    }
-  }
-
   return (
-    <TableRow>
-      <TableCell>{id}</TableCell>
-      <TableCell>{origen?.nombre}</TableCell>
-      <TableCell>{destino?.nombre}</TableCell>
+    <TableRow key={id}>
+      <TableCell>{origen.nombre}</TableCell>
+      <TableCell>{destino.nombre}</TableCell>
       <TableCell>{distancia}</TableCell>
-      <TableCell>$ {precio}</TableCell>
-      <TableCell>
-        <Checkbox
-          checked={completed}
-          onChange={() => handleComplete(id)}
-          disabled={completed}
-        />
-      </TableCell>
+      <TableCell>${precio}</TableCell>
       <TableCell>
         <IconButton
           onClick={() => handleDelete(id)}
